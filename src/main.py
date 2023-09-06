9ef47de124790ad49d11173af690593b88464ac6
Elad Levy <eladlevysignups@gmail.com>
2023-09-06 21:07:41 +0300

    Some added implementations, untested

--- src/main.py
+++ src/main.py
@@ -1,7 +1,35 @@
-from collections import namedtuple
+from __future__ import annotations
+from collections import namedtuple, defaultdict
 from typing import Dict, List
 from uuid import uuid4, UUID
 from tabulate import tabulate
+from enum import Enum
+
+
+class DiceType(Enum):
+    d4 = 4
+    d6 = 6
+    d8 = 8
+    d10 = 10
+    d12 = 12
+    d20 = 20
+    d100 = 100
+
+
+class DamageType(Enum):
+    acid = "acid"
+    bludgeoning = "bludgeoning"
+    cold = "cold"
+    fire = "fire"
+    force = "force"
+    lightning = "lightning"
+    necrotic = "necrotic"
+    piercing = "piercing"
+    poison = "poison"
+    psychic = "psychic"
+    radiant = "radiant"
+    slashing = "slashing"
+    thunder = "thunder"
 
 
 class MapItem:
@@ -20,6 +48,63 @@ class WrittenMapItem(MapItem):
 
     def __repr__(self):
         return self.__text
+
+
+AbilityScores = namedtuple("AbilityScores", ["str", "dex", "con", "int", "wis", "cha"])
+Bonus = namedtuple("Bonus", ["amount", "source"])
+
+
+class Creature(MapItem):
+    class Builder:
+        def __init__(self):
+            self.__creature: Creature = Creature()
+
+        def name(self, name: str) -> Creature:
+            self.__creature._name = name
+            return self.__creature
+
+        def level(self, level: int) -> Creature:
+            self.__creature._level = level
+            return self.__creature
+
+
+
+    def __init__(self):
+        super().__init__()
+        self._ability_scores: AbilityScores = AbilityScores(10, 10, 10, 10, 10, 10)
+        self._name: str = "Creature"
+        self._level: float = 1.0
+        self._hit_die_type: DiceType = DiceType.d8
+        self._damage_taken: int = 0
+        self._bonuses: defaultdict[str, List[Bonus]] = defaultdict(list)
+
+    def get_modifier(self, ability: str):
+        return (self._ability_scores._asdict().get(ability) - 10) // 2
+
+    @property
+    def max_hp(self) -> int:
+        return self._hit_die_type.value + \
+               (self._level-1) * ((self._hit_die_type.value // 2) + self.get_modifier("con"))
+
+    @property
+    def current_hp(self) -> int:
+        return self.max_hp - self._damage_taken
+
+    def damage(self, amount: int, dmg_type: DamageType) -> None:
+        self._damage_taken += amount
+
+    def heal(self, amount: int) -> None:
+        self._damage_taken = max(self._damage_taken - amount, 0)
+
+    def get_bonus(self, key: str) -> int:
+        return sum(bonus.amount for bonus in self._bonuses.get(key))
+
+    @property
+    def armor_class(self) -> int:
+        return 10 + self.get_bonus("armor class")
+
+    def __repr__(self):
+        return self._name
 
 
 Location = namedtuple("Location", ["x", "y"])
@@ -65,6 +150,8 @@ def main() -> None:
             y = int(input("to where? y: "))
             map.move(map.find(player), Location(x, y))
             print(map)
+        elif user_input == "melee":
+            enemy =
 
 
 if __name__ == "__main__":
