359148603a74a98403fb9bdaa4b0951feb7a9d35
Elad Levy <elad.levy2@mail.huji.ac.il>
2024-03-02 20:13:05 +0200

    tqdm works

--- src/generate_spell_cards.py
+++ src/generate_spell_cards.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 from dnd.spells import SpellBook, Spell
+from tqdm.auto import tqdm
 
 if __name__ == "__main__":
-    all_spells: SpellBook = Spell.get_all_spells()
+    all_spells: SpellBook = SpellBook.all_spells()
+    for qr in tqdm(all_spells.qr_generator(), total=len(all_spells)):
+        ...
     url: Path = all_spells.generate_html_file()
     print(url.as_uri())
